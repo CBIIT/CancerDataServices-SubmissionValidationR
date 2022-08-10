30b7fc7bb567855116f37c397524b44b6eae6440
svburke <42675174+svburke@users.noreply.github.com>
2022-08-10 14:30:05 -0500

    Update CDS-Submission_ValidationR.R

--- CDS-Submission_ValidationR.R
+++ CDS-Submission_ValidationR.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#Cancer Data Services - Submission Validation-R v1.3.3
+#Cancer Data Services - Submission Validation-R v1.3.1
 
 
 ##################
@@ -9,7 +9,7 @@
 #
 ##################
 
-#This takes a data file as input that is formatted to the submission template for CDS v1.3.
+#This takes a data file as input that is formatted to the submission template for CDS v1.3.1.
 
 #Run the following command in a terminal where R is installed for help.
 
@@ -87,6 +87,8 @@ file_name=stri_reverse(stri_split_fixed(str = stri_reverse(file_path), pattern="
 
 ext=tolower(stri_reverse(stri_split_fixed(str = stri_reverse(file_name),pattern = ".",n=2)[[1]][1]))
 
+path=paste(stri_reverse(stri_split_fixed(str = stri_reverse(file_path), pattern="/",n = 2)[[1]][2]),"/",sep = "")
+
 #Output file name based on input file name and date/time stamped.
 output_file=paste(file_name,
                   "_validation_output_",
@@ -104,7 +106,7 @@ output_file=paste(file_name,
                   sep="")
 
 #Start writing in the outfile.
-sink(output_file)
+sink(paste(path,output_file,sep = ""))
 
 cat(paste("This is a validation output for ",file_name,".\n",sep = ""))
 
@@ -128,9 +130,9 @@ expected_sheets=c("README and INSTRUCTIONS",
 #Test to see if expected sheet names are present.
 if (all(expected_sheets%in%sheet_names)){
   cat("PASS: Found expected sheets in the submitted template file.\n")
-  }else{
-    sheet_gone=1
-  }
+}else{
+  sheet_gone=1
+}
 
 #If any sheet is missing, throw an overt error message then stops the process. This script pulls information from the expected sheets and requires all sheets present before running.
 
@@ -157,18 +159,19 @@ all_properties=unique(df_dict$Field)[grep(pattern = '.',unique(df_dict$Field))]
 all_properties=all_properties[!grepl(pattern = " ",x = all_properties)]
 #Remove the Field column header that is repeated
 all_properties=all_properties[!grepl(pattern = "Field",x = all_properties)]
-#Pull out required properties
-required_properties=df_dict$Field[grep(pattern = "Yes",x = df_dict$`Required?`)]
+#Pull out required property groups
+required_property_groups=unique(df_dict$`Required?`[!is.na(df_dict$`Required?`)])
 
+required_properties=df_dict$Field[!is.na(df_dict$`Required?`)]
 
 #Read in metadata page/file to check against the expected/required properties. 
 #Further logic has been setup to accept the original XLSX as well as a TSV or CSV format.
 if (ext == "tsv"){
-  df=suppressMessages(read_tsv(file = file_path, guess_max = 1000000, col_types = cols(.default = col_character())))
+  df=suppressMessages(read_tsv(file = file_path, guess_max = 1000000, col_types = cols(.default = col_character(),),trim_ws = FALSE))
 }else if (ext == "csv"){
-  df=suppressMessages(read_csv(file = file_path, guess_max = 1000000, col_types = cols(.default = col_character())))
+  df=suppressMessages(read_csv(file = file_path, guess_max = 1000000, col_types = cols(.default = col_character()), trim_ws = FALSE))
 }else if (ext == "xlsx"){
-  df=suppressMessages(read_xlsx(path = file_path,sheet = "Metadata", guess_max = 1000000, col_types = "text"))
+  df=suppressMessages(read_xlsx(path = file_path,sheet = "Metadata", guess_max = 1000000, col_types = "text",trim_ws = FALSE))
 }else{
   stop("\n\nERROR: Please submit a data file that is in either xlsx, tsv or csv format.\n\n")
 }
@@ -178,16 +181,16 @@ col_gone=0
 
 if (all(all_properties%in%colnames(df))){
   cat("PASS: Found expected columns for the Metadata sheet.\n")
-  }else{
-    col_gone=1
-    missing=all_properties[!all_properties%in%colnames(df)]
-    for (missing_prop in missing){
-      if (missing_prop%in%required_properties){
-        cat(paste("ERROR: The Metadata sheet is missing the following required column: ",missing_prop,".\n",sep = ""))
-      }else{
-        cat(paste("ERROR: The Metadata sheet is missing the following column: ",missing_prop,".\n",sep = ""))
-      }
+}else{
+  col_gone=1
+  missing=all_properties[!all_properties%in%colnames(df)]
+  for (missing_prop in missing){
+    if (missing_prop%in%required_properties){
+      cat(paste("ERROR: The Metadata sheet is missing the following required column: ",missing_prop,".\n",sep = ""))
+    }else{
+      cat(paste("ERROR: The Metadata sheet is missing the following column: ",missing_prop,".\n",sep = ""))
     }
+  }
   
   #Make it very obvious that the user should not continue with the current template file as it does not contain all expected information.
   if (col_gone==1){
@@ -202,36 +205,89 @@ if (all(all_properties%in%colnames(df))){
 #
 ####################
 
-#For the required columns that are present, it will check to make sure there are no completely empty columns. If there are only partially filled required columns or the values within the required column contain leading and/or trailing white space, it will return those row positions for the required columns.
-for (property in required_properties){
-  if (property%in%colnames(df)){
-    #Return if required column is empty
-    if (all(is.na(df[property]))){
-      cat(paste("ERROR: All values are missing for the required ",property," property, on the Metadata sheet. Please enter values in this property.\n", sep = ""))
-    }else{
+cat("\nThis section is for required properties and their required groups, seen on the 'Dictionary' page of the template file.\nFor each row entry, if any value is in a required column, then all properties for that required group must also have values for that row entry:\n\n")
+
+#For blocks of required columns, it will check if all required columns have values if any one required column have a value. If there are only partially filled rows for required columns or the values within the required column contain leading and/or trailing white space, it will return those row positions for the required columns.
+
+for (required_property_group in required_property_groups){
+  
+  required_properties_dict=df_dict$Field[grepl(pattern = required_property_group,x = df_dict$`Required?`)]
+  required_properties=colnames(df[colnames(df)%in%required_properties_dict])
+  
+  #Clean up the data frame, if the required property section was left completely blank, it is assumed that the user did not have data for that row. Thus, it will only compare to rows that have complete data.
+  df_property_clean=remove_empty(df[required_properties],c('cols','rows'))
+  
+  #Counter used later for ignoring purposely empty groups of required information
+  incomplete_required_group=0
+  
+  #Test to see if the number of columns are the same or if a complete property was ignored.
+  if (dim(df_property_clean)[2]!=dim(df[required_properties])[2]){
+    if (!all(is.na(df[required_properties]))){
+      missing_col=colnames(df[required_properties][!colnames(df[required_properties])%in%colnames(df_property_clean)])
+      cat(paste("ERROR: Column from a required group of properties, ",required_property_group,", was found empty on the Metadata sheet. Please add values for the following columns: ", missing_col,"\n", sep = ""))
+    }
+    
+    #Test to see if there are complete cases, values for all properties.    
+  }else if(!all(complete.cases(df[required_properties]))){
+    for (row in 1:dim(df[required_properties])[1]){
+      if (!complete.cases(df[required_properties][row,])){
+        #if a row was left completely blank, assumed to be on purpose, then it will be skipped.
+        if (!all(is.na(df[required_properties][row,]))){
+          cat(paste("ERROR: Missing values were found in the required property group ",required_property_group,", on the Metadata sheet. Please check the following row: ", row,"\n", sep = ""))
+          incomplete_required_group=1
+        }
+      }
+    }
+    #if there was not one instance of tripping the incomplete_required_group flag, then it will print that values are present for complete rows of required data for a required group.
+    if (incomplete_required_group==0){
+      cat(paste("WARNING: Required property group ",required_property_group," contains values for all entries that are assumed to have values based on the submitted data structure.\n",sep = ""))
+    }
+  }else{
+    #if none of these situations are triggered, then the column likely has all values present in the required property group for each entry.
+    cat(paste("PASS: Required property group ",required_property_group," contains values for all expected entries.\n",sep = ""))
+  }
+  
+  #This section will check against white space in the values of each column when there is a value present.
+  for (property in required_properties){
+    if (property%in%colnames(df)){
       df_temp=df
+      incomplete_required_property=0
       for (x in 1:dim(df[property])[1]){
-      df_temp[property][x,]=trimws(df[property][x,])
+        df_temp[property][x,]=trimws(df[property][x,])
       }
       #Return value positions that are either empty (NA) or contain leading/trailing white space in the value for the required column.
       if (!all(df_temp[property]==df[property]) | any(is.na(df_temp[property]==df[property]))){
         position_mis=grep(pattern = FALSE, x = df_temp[property]==df[property])
         position_na=grep(pattern = TRUE, x=(is.na(df_temp[property]==df[property])))
         position=c(position_mis,position_na)
-        cat(paste("ERROR: Missing values and/or leading/trailing white space in the required ",property," property, on the Metadata sheet. Please check the following position: ", position,"\n", sep = ""))
+        for (instance in position){
+          if (!all(is.na(df_temp[required_properties][instance,]))){
+            cat(paste("ERROR: Missing values and/or leading/trailing white space in the required ",property," property, on the Metadata sheet. Please check the following position: ", instance,"\n", sep = ""))
+            incomplete_required_property=1
+          }
+        }
+        #Returns a warning that there is likely empty rows on purpose due to not all entries having a value for that section of required input.
+        if (incomplete_required_property==0 & !all(is.na(df[required_properties]))){
+          cat(paste("WARNING: Required property ",property," contains values for all entries that are assumed to have values based on the submitted data structure.\n",sep = ""))
+        }
       }else{
         #Required column contains values for each entry.
-      cat(paste("PASS: Required property ",property," contains values for all entries.\n",sep = ""))
+        if(incomplete_required_property==0 & incomplete_required_group==0){
+          cat(paste("PASS: Required property ",property," contains values for all expected entries.\n",sep = ""))
+        }
       }
     }
   }
 }
+
 
 ##################
 #
 # Terms and Value sets
 #
 ##################
+
+cat("\nThe following columns have controlled vocabulary on the 'Terms and Value Sets' page of the template file:\n\n")
 
 #Read in Terms and Value sets page to obtain the required value set names.
 df_tavs=suppressMessages(read_xlsx(path = template_path, sheet = "Terms and Value Sets"))
@@ -246,6 +302,7 @@ df_all_terms=list()
 for (x in 1:length(VSN)){
   if (!is.na(VSN[x+1])){
     df_all_terms[as.character(df_tavs[VSN[x],1])] = as.vector(df_tavs[VSN[x]:(VSN[x+1])-1,3])
+    
   }else{
     df_all_terms[as.character(df_tavs[VSN[x],1])] = as.vector(df_tavs[VSN[x]:dim(df_tavs)[1],3])
   }
@@ -254,17 +311,21 @@ for (x in 1:length(VSN)){
 #Use the list of all accepted values for each value_set_name, and compare that against the Metadata page and determine if the values, if present, match the accepted terms.
 for (value_set_name in names(df_all_terms)){
   if (value_set_name%in%colnames(df)){
-    if (!all(((unique(df[value_set_name][[1]]))%in%df_all_terms[value_set_name][[1]]))){
-      for (x in 1:dim(unique(df[value_set_name]))[1]){
-        check_value=unique(df[value_set_name])[x,]
-        if (!is.na(check_value)){
-          if (!grepl(pattern = check_value, x = df_all_terms[value_set_name])){
-            cat(paste("ERROR: ",value_set_name," property contains a value that is not recognized: ", check_value,"\n",sep = ""))
+    unique_values=unique(df[value_set_name][[1]])
+    unique_values=unique_values[!is.na(unique_values)]
+    if (length(unique_values)>0){
+      if (!all(unique_values%in%df_all_terms[value_set_name][[1]])){
+        for (x in 1:dim(unique(df[value_set_name]))[1]){
+          check_value=unique(df[value_set_name])[x,]
+          if (!is.na(check_value)){
+            if (!as.character(check_value)%in%df_all_terms[value_set_name][[1]]){
+              cat(paste("ERROR: ",value_set_name," property contains a value that is not recognized: ", check_value,"\n",sep = ""))
+            }
           }
         }
+      }else{
+        cat(paste("PASS:",value_set_name,"property contains all valid values.\n"))
       }
-    }else{
-      cat(paste("PASS:",value_set_name,"property contains all valid values.\n"))
     }
   }
 }
@@ -279,4 +340,4 @@ for (value_set_name in names(df_all_terms)){
 #Stop write out to file and display "done message" on command line.
 sink()
 
-cat(paste('Please see the following file, ',output_file,', to determine which parts passed or contained errors.\n',sep = ""))
+cat(paste('Please see the following file, ',output_file,', to determine which parts passed, had warnings, or contained errors.\n',sep = ""))
