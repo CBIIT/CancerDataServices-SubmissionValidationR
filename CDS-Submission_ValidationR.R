6fc2082199c757368929181d32b4571ddd81b3e7
svburke <42675174+svburke@users.noreply.github.com>
2022-10-27 14:03:35 -0500

    Update CDS-Submission_ValidationR.R

--- CDS-Submission_ValidationR.R
+++ CDS-Submission_ValidationR.R
@@ -58,7 +58,7 @@ option_list = list(
 )
 
 #create list of options and values for file input
-opt_parser = OptionParser(option_list=option_list, description = "\nCDS-Submission_ValidationR v.1.3.3")
+opt_parser = OptionParser(option_list=option_list, description = "\nCDS-Submission_ValidationR v2.0.0")
 opt = parse_args(opt_parser)
 
 #If no options are presented, return --help, stop and print the following message.
