a5a7d891bbc2b3725b761156f753f76a8a871f5a
svburke <sean.burke2@nih.gov>
2023-04-27 15:15:34 -0500

    Update CDS-Submission_ValidationR.R

--- CDS-Submission_ValidationR.R
+++ CDS-Submission_ValidationR.R
@@ -651,7 +651,7 @@ for (bucket_num in 1:dim(df_bucket)[1]){
     #if the file is found, find that file with the correct size
     if (length(file_name_loc)!=0){
       if (bucket_metadata[file_name_loc,'file_size']!=df_bucket_specific[row,'file_size'][[1]]){
-        cat(paste("ERROR: The following file does not have the same file size found in the AWS bucket: ", df_bucket_specific[row,'file_url_in_cds'][[1]],"\n", sep = ""))
+        cat(paste("ERROR: The following file does not have the same file size found in the AWS bucket: ", df_bucket_specific[row,'file_url_in_cds'][[1]],"\n\t\tBucket:",bucket_metadata[file_name_loc,'file_size']," != Manifest:",as.character(df_bucket_specific[row,'file_size'][[1]]),"\n", sep = ""))
       }
     }else{
       cat(paste("ERROR: The following file is not found in the AWS bucket: ", df_bucket_specific[row,'file_url_in_cds'][[1]],"\n", sep = ""))
