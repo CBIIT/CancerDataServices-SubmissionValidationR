be9c317581a9fdaae2760de0e5cb17e6eeb2ad02
svburke <42675174+svburke@users.noreply.github.com>
2023-05-01 13:50:20 -0500

    Update CDS-Submission_ValidationR.R

--- CDS-Submission_ValidationR.R
+++ CDS-Submission_ValidationR.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#Cancer Data Services - Submission Validation-R v2.0.1
+#Cancer Data Services - Submission Validation-R
 
 
 ##################
@@ -60,7 +60,7 @@ option_list = list(
 )
 
 #create list of options and values for file input
-opt_parser = OptionParser(option_list=option_list, description = "\nCDS-Submission_ValidationR v2.0.3")
+opt_parser = OptionParser(option_list=option_list, description = "\nCDS-Submission_ValidationR v2.0.4")
 opt = parse_args(opt_parser)
 
 #If no options are presented, return --help, stop and print the following message.
